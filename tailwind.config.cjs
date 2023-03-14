70150ab1521b38ddcb795ec4cbaf26ed417a9d38
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-14 14:55:14 +0000

    refactor(build): add ts file paths, update color names
    1. Ts files paths needed for tailwind to compile css.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: [
-    'templates/*.html'
-  ],
+  content: ['./templates/**/*.html', './static/src/js/**/*.ts'],
   theme: {
     container: {
       center: true,
@@ -10,19 +8,18 @@ module.exports = {
         DEFAULT: '1rem',
         sm: '2rem',
         lg: '4rem',
-        xl: '5rem'
-
-      }
-
+        xl: '5rem',
+      },
     },
     extend: {
       colors: {
         primary: 'hsl(var(--color-primary) / <alpha-value>)',
         tertiary: 'hsl(var(--color-tertiary) / <alpha-value>)',
-        black: 'hsl(var(--color-black) / <alpha-value>)',
-        gray: 'hsl(var(--color-gray) / <alpha-value>)'
-      }
-    }
+        'black-base': 'hsl(var(--color-black-base) / <alpha-value>)',
+        'gray-base': 'hsl(var(--color-gray-base) / <alpha-value>)',
+      },
+    },
+
   },
-  plugins: []
+  plugins: [],
 }
