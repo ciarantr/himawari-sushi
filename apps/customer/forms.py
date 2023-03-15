cca9f4cce4c870987f5357e9331f77d7514c5409
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-15 12:30:34 +0000

    feat(forms): add customer form model

---
+++ apps/customer/forms.py
@@ -0,0 +1,78 @@
+from django import forms
+from django.contrib.auth.models import User
+from ..validators.customer_validators import *
+
+
+class CustomerUpdateForm(forms.ModelForm):
+    # A form for updating the customer details
+    # This form inherits from the User model
+    # & adds the phone number field
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+        widgets = {
+            'username': forms.TextInput(
+                attrs={'pattern': username_pattern['pattern'],
+                       'title': username_pattern['title']
+                       },
+            ),
+
+            'first_name': forms.TextInput(
+                attrs={'pattern': name_part_pattern['pattern'],
+                       'title': name_part_pattern['title']
+                       },
+            ),
+
+            'last_name': forms.TextInput(
+                attrs={'pattern': name_part_pattern['pattern'],
+                       'title': name_part_pattern['title']
+                       },
+            ),
+
+            'email': forms.EmailInput(
+                attrs={'pattern': email_pattern['pattern'],
+                       'title': email_pattern['title']
+                       },
+            ),
+
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomerUpdateForm, self).__init__(*args, **kwargs)
+
+        if hasattr(self.instance, 'customer'):
+            # add phone number field to form if user has customer object
+            self.fields['phone_number'] = forms.CharField(
+                label='Phone Number',
+                required=False,
+                validators=[validate_phone_number],
+                max_length=20,
+            )
+            self.fields[
+                'phone_number'].initial = self.instance.customer.phone_number
+            self.fields['phone_number'].widget = forms.TextInput(
+                attrs={'pattern': phone_number_pattern['pattern'],
+                       'title': phone_number_pattern['title']
+                       },
+            )
+
+        # extent form validators
+        self.fields['first_name'].validators.append(validate_part_name)
+        self.fields['last_name'].validators.append(validate_part_name)
+        self.fields['email'].validators.append(validate_email)
+
+    def save(self, commit=True):
+        # check if user has customer object and
+        # if so, update user & customer info
+        user = super(CustomerUpdateForm, self).save(commit=False)
+        if hasattr(self.instance, 'customer'):
+            user.customer.phone_number = self.cleaned_data['phone_number']
+            if commit:
+                user.save()
+                user.customer.save()
+        else:
+            if commit:
+                user.save()
+        return user
