87edc07e65af282a580230c7d4f27e3ffa0be232
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-15 12:44:29 +0000

    feat(urls): add app urls

--- sushi/urls.py
+++ sushi/urls.py
@@ -19,4 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.home.urls'), name='home'),
+    path('', include('apps.accounts.urls')),
+    path('', include('apps.bookings.urls')),
+    path('', include('apps.customer.urls')),
 ]
