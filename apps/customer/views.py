f4a94fc709442b9370fe0f83b4b3540b2bb2dc40
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-15 12:23:24 +0000

    refactor(view): abstract  request  to static function
    1. Booking request will be used in multiple views

--- apps/customer/views.py
+++ apps/customer/views.py
@@ -6,19 +6,29 @@ from apps.bookings.models import Booking
 
 # Create your views here.
 
-class CustomerDashboard(View):
-    # A view to display the customer's bookings
-    def get(self, request):
-        user = User.objects.get(username=request.user)
-        # query user's bookings
-        # check if the user has a customer profile
-        if hasattr(user, 'customer'):
-            # query the bookings table for all bookings with the current user
-            bookings = Booking.get_booking_information(user)
-        else:
-            bookings = None
+def booking_request(template, request) -> render:
+    # A function to query the bookings table for the current user
+    # and return the results to the template
 
-        context = {'bookings': bookings}
-        template = 'profile/index.html'
+    user = User.objects.get(username=request.user)
+    # query user's bookings
+    # check if the user has a customer profile
+    if hasattr(user, 'customer'):
+        # query the bookings table for all bookings with the current user
+        bookings = Booking.get_booking_information(user)
+    else:
+        bookings = None
+
+    context = {'bookings': bookings}
+
+    return render(request, template, context)
+
+
+class CustomerDashboard(View):
+    # A view to display the customer's bookings overview
+    def get(self, request):
+        return booking_request('profile/index.html', request)
+
+
         return render(request, template, context)
 
