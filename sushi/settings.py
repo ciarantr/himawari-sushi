5f2310607cff814fd3bbc44ac58f23b37cb753cc
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-15 16:13:14 +0000

    fix(settings): development should return boolean

--- sushi/settings.py
+++ sushi/settings.py
@@ -15,7 +15,7 @@ import os
 
 if os.path.isfile('env.py'):
     import env
-development = os.environ.get('DEVELOPMENT', True)
+development = os.environ.get('DEVELOPMENT') == 'True'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
