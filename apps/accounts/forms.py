812eccf81a49e178ea64d29c6d917b967ec2f058
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-28 12:22:15 +0100

    fix(forms): user registered email not added to db on signup
    1. Add emailField to form, UserCreationForm does not inherently have email- filed.

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -3,21 +3,29 @@ from django.contrib.auth.models import User
 from django.forms.widgets import TextInput, EmailInput
 from django.utils.translation import gettext_lazy as _
 from ..validators.customer_validators import email_pattern, username_pattern
+from ..validators.customer_validators import validate_email
+from django import forms
 
 
 class SignupForm(UserCreationForm):
+    email = forms.EmailField(required=True,
+                             validators=[validate_email], max_length=100)
+
+    email.widget.attrs.update({
+        'pattern': email_pattern['pattern'],
+        'title': email_pattern['message'],
+    })
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2',)
         labels = {
             'username': _('User name'),
-            'email': _('Email'),
             'password1': _('Password'),
             'password2': _('Password confirmation'),
         }
         help_texts = {
             'username': None,
-            'email': None,
             'password1': None,
             'password2': None,
         }
@@ -26,7 +34,4 @@ class SignupForm(UserCreationForm):
             'username': TextInput(attrs={'pattern': username_pattern['pattern'],
                                          'title': username_pattern['message'],
                                          }),
-            'email': EmailInput(attrs={'pattern': email_pattern['pattern'],
-                                       'title': email_pattern['message'],
-                                       }),
         }
