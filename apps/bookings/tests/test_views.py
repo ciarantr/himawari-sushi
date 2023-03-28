d475370fd4a4f9c28e67031483cb893ab506d5af
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-28 13:40:53 +0100

    test(views): test booking creation

--- apps/bookings/tests/test_views.py
+++ apps/bookings/tests/test_views.py
@@ -33,3 +33,22 @@ class TestViews(TestCase):
         response = self.client.get(self.booking_create_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'bookings/booking.html')
+
+    def test_can_create_booking(self):
+        # Create a booking
+        response = self.client.post(self.booking_create_url, {
+            'booking_date': today_date,
+            'booking_time': '13:00',
+            'placements': '2',
+            'message': 'Test booking message',
+        })
+        # Check if booking was created and redirect to profile page
+        self.assertRedirects(response, self.profile_url)
+        self.assertEquals(Booking.objects.count(), 1)
+        self.assertEquals(
+            Booking.objects.get().booking_date, today_date)
+        self.assertEquals(Booking.objects.get().booking_time.strftime('%H:%M'),
+                          '13:00')
+        self.assertEquals(Booking.objects.get().placements, 2)
+        self.assertEquals(Booking.objects.get().message, 'Test booking message')
+        self.assertEquals(Booking.objects.get().customer, self.customer)
