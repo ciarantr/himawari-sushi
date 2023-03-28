161426fe8f764bc9e5ab8905165d9516addc5a12
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-28 12:08:03 +0100

    test(views): add setup for testcase views

---
+++ apps/accounts/tests/test_views.py
@@ -0,0 +1,26 @@
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.username = "testuser"
+        self.password = "testpassword"
+        self.email = "test@gmail.com"
+        self.password = "testpassword"
+        self.password2 = "testpassword"
+
+        self.client = Client()
+        self.login_url = reverse('login')
+        self.logout_url = reverse('logout')
+        self.register_url = reverse('register')
+        self.user = User.objects.create_user(
+            username=self.username,
+            password=self.password,
+        )
+
+
+
