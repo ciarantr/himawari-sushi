c11b65ef43d0c661aac22064ff17db5195cc956f
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-28 18:30:10 +0100

    test(urls): add unit test for  customer app urls
    1. Test urls resolve to correct Views.

---
+++ apps/customer/tests/test_urls.py
@@ -0,0 +1,19 @@
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
+
+from apps.accounts.views import CreateUser, LoginUser, LogoutUser
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_login_url_is_resolved(self):
+        url = reverse('login')
+        self.assertEquals(resolve(url).func.view_class, LoginUser)
+
+    def test_logout_url_is_resolved(self):
+        url = reverse('logout')
+        self.assertEquals(resolve(url).func.view_class, LogoutUser)
+
+    def test_register_url_is_resolved(self):
+        url = reverse('register')
+        self.assertEquals(resolve(url).func.view_class, CreateUser)
