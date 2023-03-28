50b89787259b00d2d0831d3022c3f9ac07d2f1ed
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-28 12:44:24 +0100

    test:(forms): test fail invalid times entered
    1. Test invalid time entry.
    2. Test form errors length.
    3. Test form error message returned.

--- apps/bookings/tests/test_forms.py
+++ apps/bookings/tests/test_forms.py
@@ -32,3 +32,16 @@ class TestBookingForm(TestCase):
             form.errors['booking_date'][0],
             'We can only accept bookings up to the end of the year'
         )
+
+    def test_fail_booking_form_time_out_of_range(self):
+        form = BookingForm(data={
+            'booking_date': today_date,
+            'booking_time': '12:00',
+            'placements': '2',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 1)
+        self.assertEquals(
+            form.errors['booking_time'][0],
+            'Please select a time between 13:00 p.m and 21:00 p.m'
+        )
