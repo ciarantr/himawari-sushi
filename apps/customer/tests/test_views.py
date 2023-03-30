55c2435fc02e83c50ea9ac7f5411dc3b67cbd04a
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-30 12:50:46 +0100

    test(views): test profile edit updates customer details

--- apps/customer/tests/test_views.py
+++ apps/customer/tests/test_views.py
@@ -32,3 +32,23 @@ class TestViews(TestCase):
         response = self.client.get(reverse('profile-details'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'profile/account.html')
+
+    def test_POST_customer_profile_edit_page(self):
+        response = self.client.post(reverse('profile-details'), {
+            'username': 'Cal',
+            'first_name': 'Callum',
+            'last_name': 'Peterson',
+            'email': 'newemail@gmail.com',
+            'phone_number': '656565632'
+        })
+        # refresh customer object
+        self.customer.refresh_from_db()
+        # check if customer details are updated
+        self.assertEquals(self.customer.customer.username, 'Cal')
+        self.assertEquals(self.customer.customer.first_name, 'Callum')
+        self.assertEquals(self.customer.customer.last_name, 'Peterson')
+        self.assertEquals(self.customer.customer.email, 'newemail@gmail.com')
+        self.assertEquals(self.customer.phone_number, '656565632')
+        # check if redirect to profile page
+        self.assertEquals(response.status_code, 302)
+        self.assertRedirects(response, reverse('profile-details'))
