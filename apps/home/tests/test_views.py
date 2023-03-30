3f9def94ea1d8790463626c68e7636b135fac623
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-30 14:57:27 +0100

    test(views): test about page template used & status code is correct

--- apps/home/tests/test_views.py
+++ apps/home/tests/test_views.py
@@ -8,3 +8,8 @@ class TestViews(TestCase):
         url = self.client.get('/')
         self.assertEqual(url.status_code, 200)
         self.assertTemplateUsed(url, 'index.html')
+
+    def test_GET_about_page(self):
+        url = self.client.get('/about/')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'about.html')
