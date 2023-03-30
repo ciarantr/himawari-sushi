19029b2bd9bcab8f3dde98d6b23f440fe3fb95ed
Ciaran Toner <58338331+ciaran-io@users.noreply.github.com>
2023-03-30 12:31:25 +0100

    test:(views) test correct template use for profile page

---
+++ apps/customer/tests/test_views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+from ..models import Customer
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.new_customer = Customer.objects.create(
+            customer=User.objects.create_user(
+                username='testuser',
+                password='testpassword',
+            )
+        )
+        # get customer by username
+        self.customer = Customer.objects.get(customer__username='testuser')
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_GET_customer_profile_page(self):
+        response = self.client.get(reverse('profile'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profile/index.html')
